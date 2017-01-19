f22227a79bf0b992a9d764c10519dddd8ed72238
Adrien Dubois <adrien.dubois.be@gmail.com>
2017-01-19 17:37:26 +0100

    Creating chef routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
     collection do                       # collection => no bar id in URL. Gives something like GET /restaurants/top
       get 'top', to: "bars#top"         # barsController#top
     end
+    member do                             # member => bar id in URL
+      get 'chef', to: "bars#chef"         # barsController#chef
+    end
   end
 end
